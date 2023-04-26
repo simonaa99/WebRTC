9da3cf2ac31fddbde607c9ddc4fc308df43e8d42
simonaa99 <simona.baosic@gmail.com>
2023-04-26 16:23:52 +0200

    Fixed glitch with video closing

--- public/script.js
+++ public/script.js
@@ -1,62 +1,74 @@
-//import {Peer} from 'peerjs'
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-    host:'/',
-    port: '3001'
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
+// import { Peer } from "peerjs";
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer();
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
-const peers = {}
+const peers = {};
 
-navigator.mediaDevices.getUserMedia({
-    video:true,
-    audio:true
-}).then(stream =>{
-    addVideoStream(myVideo, stream)/*vidimo video na stranici*/
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream); /vidimo video na stranici/
 
-    myPeer.on('call', call=>{   /*receive calls*/
-        call.answer(stream)
-        const video = document.createElement('video')
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
-        })
-    })
+    myPeer.on("call", (call) => {
+      /receive calls/
+      call.answer(stream);
 
-    socket.on('user-connected', userId=>{
-        connectToNewUser(userId, stream)
-    })
+      const video = document.createElement("video");
+      video.className = call.peer;
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
 
-    
-})
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
 
-socket.on('user-disconnected',userId =>{
-    if(peers[userId])peers[userId].close()
-})
+socket.on("user-disconnected", (userId) => {
+  const relatedVideo = document.getElementsByClassName(userId.toString());
+  relatedVideo[0].remove();
+  peers[userId].close();
+});
 
-myPeer.on('open', id =>{
-    socket.emit('join-room',ROOM_ID, id)
-})
+myPeer.on("open", (id) => {
+  myVideo.className = myPeer._id;
+  socket.emit("join-room", ROOM_ID, id);
+});
 
-function connectToNewUser(userId, stream){/*make calls */
-    const call = myPeer.call(userId, stream)
-    const video = document.createElement('video')
-    call.on('stream', userVideoStream =>{
-        addVideoStream(video, userVideoStream)
-    })
-    call.on('close', ()=>{
-        video.remove()
-    })
+function connectToNewUser(userId, stream) {
+  /*make calls */
+  const call = myPeer.call(userId, stream);
 
-    peers[userId] = call
+  const video = document.createElement("video");
+
+  video.className = userId.toString();
+
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream, userId);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+  video.className = userId;
 }
 
-function addVideoStream(video, stream){
-    video.srcObject = stream //allow us to play the video
-    video.addEventListener('loadedmetadata',()=>{
-        video.play()
-    }) //ucitani video pusta na stranici
-    videoGrid.append(video)//stavlja video na nas grid
+function addVideoStream(video, stream, userId) {
+  video.srcObject = stream; //allow us to play the video
+  if (userId) {
+    video.className = userId.toString();
+  }
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  }); //ucitani video pusta na stranici
+
+  videoGrid.append(video); //stavlja video na nas grid
 }
-  
